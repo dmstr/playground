8b3ef2bd4217ba9f62a61ab4402d9f64f8bc452a
Tobias Munk <schmunk@usrbin.de>
2015-02-18 17:26:16 +0100

    updated image names

--- build/docker-build.sh
+++ build/docker-build.sh
@@ -1,5 +1,5 @@
 VERSION=`git describe --tags``test -z "$(git status --porcelain)" || echo "-dirty"`
 echo $VERSION > version
 
-docker build -f Dockerfile-production -t myapp-production .
-docker build -f Dockerfile -t myapp-development .
+docker build -f Dockerfile-production -t playground:production .
+docker build -f Dockerfile -t playground:development .
--- build/docker-test.sh
+++ build/docker-test.sh
@@ -3,8 +3,8 @@ echo $VERSION > version
 
 docker-compose build testweb
 docker-compose up -d testweb
-docker exec app_testweb_1 codecept build
+docker exec playground_testweb_1 codecept build
 # wait for webserver
 sleep 10
-docker exec app_testweb_1 codecept run
+docker exec playground_testweb_1 codecept run
 docker-compose stop testweb testdb
