100f7b6108970ae531b2e022362b84ac4c579e78
Tobias Munk <schmunk@usrbin.de>
2015-03-06 03:47:11 +0100

    regenerated CRUD

--- modules/admin/views/default/index.php
+++ modules/admin/views/default/index.php
@@ -66,7 +66,7 @@
 
     <div class="col-md-3 col-xs-6">
         <!-- small box -->
-        <div class="small-box bg-<?= strstr(getenv('APP_VERSION'),'dirty')?'orange':'green' ?>">
+        <div class="small-box bg-<?= strstr(getenv('APP_VERSION'),'-dev')?'orange':'green' ?>">
             <div class="inner">
                 <h3>
                     <?= YII_ENV ?>
